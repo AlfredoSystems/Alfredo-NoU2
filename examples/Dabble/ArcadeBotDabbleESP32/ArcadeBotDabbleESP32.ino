5f0dec3293d12e567ebefd5ba45a566d3d66b1c3
BotSpace <jrw4561@gmail.com>
2023-06-20 02:15:45 -0400

    Update ArcadeBotDabbleESP32.ino

--- examples/Dabble/ArcadeBotDabbleESP32/ArcadeBotDabbleESP32.ino
+++ examples/Dabble/ArcadeBotDabbleESP32/ArcadeBotDabbleESP32.ino
@@ -1,15 +1,15 @@
 /**
- * Example code for a robot using an ESP32 and NoU2 controlled with the dabble app.
+ * Example code for a robot using an ESP32 and NoU2 controlled with the Dabble app.
  * The NoU2 library can be found at https://github.com/AlfredoElectronics/Alfredo-NoU2.
- *
- * Detailed instructions can be found at https://github.com/AlfredoElectronics/AlfredoConnect-Receive/.
+ * 
+ * Dabble is avalible on IOS https://apps.apple.com/us/app/dabble-bluetooth-controller/id1472734455
+ * and Android https://play.google.com/store/apps/details?id=io.dabbleapp&hl=en_AU&gl=US
  */
 
-#include <AlfredoConnect.h>
-#include <BluetoothSerial.h>
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
+#include <DabbleESP32.h>
 #include <Alfredo_NoU2.h>
-
-BluetoothSerial bluetooth;
 
 // If your robot has more than a drivetrain and one servo, add those actuators here 
 NoU_Motor frontLeftMotor(1);
@@ -22,11 +22,9 @@ NoU_Servo servo(1);
 NoU_Drivetrain drivetrain(&frontLeftMotor, &frontRightMotor, &rearLeftMotor, &rearRightMotor);
 
 void setup() {
-//EVERYONE SHOULD CHANGE "ESP32 Bluetooth" TO THE NAME OF THEIR ROBOT HERE BEFORE PAIRING THEIR ROBOT TO ANY LAPTOP
-    bluetooth.begin("ESP32 Bluetooth");
-    AlfredoConnect.begin(bluetooth);
-    bluetooth.println("ArcadeBotGamepad started");
-
+//EVERYONE SHOULD CHANGE "ESP32-Bluetooth" TO THE NAME OF THEIR ROBOT HERE BEFORE PAIRING THEIR ROBOT TO ANY LAPTOP
+    Dabble.begin("ESP32-Bluetooth");       //set bluetooth name of your device
+    Serial.begin(115200);
 // If a motor in your drivetrain is spinning the wrong way, change the value for it here from 'false' to 'true'
     frontLeftMotor.setInverted(false);
     frontRightMotor.setInverted(true);
@@ -45,10 +43,13 @@ void loop() {
     int servoAngle = 0;
 
 // Here we decide what the throttle and rotation direction will be based on gamepad inputs   
-    if (AlfredoConnect.getGamepadCount() >= 1) {
-        float throttle = -AlfredoConnect.getAxis(0, 1);
-        float rotation = AlfredoConnect.getAxis(0, 0);
-        
+    if (Dabble.isAppConnected()) {
+        float throttle = scaleDabbleYaxis(GamePad.getYaxisData());
+        float rotation = scaleDabbleXaxis(GamePad.getXaxisData());
+        Serial.print(throttle);
+        Serial.print(" ");
+        Serial.println(rotation);
+
         drivetrain.arcadeDrive(throttle, rotation);
 
         RSL::setState(RSL_ENABLED);
@@ -56,18 +57,39 @@ void loop() {
         RSL::setState(RSL_DISABLED);
     }
 
-// Here we decide what the servo angle will be based on if the Q key is pressed ()
-    if (AlfredoConnect.keyHeld(Key::Q)) {
+// functions you can call to get controller state from your phone
+// GamePad.isUpPressed()
+// GamePad.isDownPressed()
+// GamePad.isLeftPressed()
+// GamePad.isRightPressed()
+// GamePad.isSquarePressed()
+// GamePad.isCirclePressed()
+// GamePad.isCrossPressed()
+// GamePad.isTrianglePressed()
+// GamePad.isStartPressed()
+// GamePad.isSelectPressed()
+
+// Here we decide what the servo angle will be based on if the circle button is pressed
+    if (GamePad.isCirclePressed()) {
         servoAngle = 50;
     }
     else {
-        servoAngle = 110;
+        servoAngle = 140;
     }
 
-// Here we set the drivetrain motor speeds and servo angle based on what we found in the above code
+// Here we set the servo angle based on what we found in the above code
     servo.write(servoAngle);
 
 // No need to mess with this code
-    AlfredoConnect.update();
+    Dabble.processInput(); 
     RSL::update();
+}
+
+//Dabble axis values are weird, these helper funtions scale them to be useful to the NoU2 library
+float scaleDabbleYaxis(float y_in) {
+  return (0 < y_in) ? (y_in/7) : (y_in/6);
+}
+
+float scaleDabbleXaxis(float x_in) {
+  return (0 < x_in) ? (x_in/6) : (x_in/7);
 }
